8e9521d44c4d2a4e47544e712619d63c683fe648
randallkanna <randallkanna@gmail.comm>
2019-03-20 16:52:02 -0700

    fixes schema

--- server/models/person.js
+++ server/models/person.js
@@ -10,7 +10,12 @@ var personSchema = new Schema({
     hair: String,
     skin_color: String,
     eye_color: String,
-    gender: String,
+    gender: [String],
+    homeworld: [String],
+    films: [String],
+    species: [String],
+    vehicles: [String],
+    starships: [String],
 })
 
 export default mongoose.model('person', personSchema);
--- server/populate.js
+++ server/populate.js
@@ -3,27 +3,46 @@ import Person from './models/person';
 
 const people = [
     {
-        name: 'Luke',
+        name: "Luke",
         height: "172",
         mass: "77",
         hair: "fair",
         skin_color: "fair",
         eye_color: "blue",
+        gender: "male",
+        homeworld: "https://swapi.co/api/planets/1/",
+        films: ["https://swapi.co/api/films/2/"],
+        species: ["https://swapi.co/api/species/1/"],
+        vehicles: ["https://swapi.co/api/vehicles/14/"],
+        starships: ["https://swapi.co/api/vehicles/14/"],
+    }, {
+        name: "Leia",
+        height: "120",
+        mass: "44",
+        hair: "brown",
+        skin_color: "fair",
+        eye_color: "blue",
         gender: "female",
-        // homeworld: "https://swapi.co/api/planets/1/",
-        // films: ["https://swapi.co/api/films/2/"],
-        // species: ["https://swapi.co/api/species/1/"]
+        homeworld: "https://swapi.co/api/planets/1/",
+        films: ["https://swapi.co/api/films/2/"],
+        species: ["https://swapi.co/api/species/1/"],
+        vehicles: ["https://swapi.co/api/vehicles/14/"],
+        starships: ["https://swapi.co/api/vehicles/14/"],
+    }, {
+        name: "Chewy",
+        height: "350",
+        mass: "250",
+        hair: "brown",
+        skin_color: "hairy",
+        eye_color: "brown",
+        gender: "male",
+        homeworld: "https://swapi.co/api/planets/1/",
+        films: ["https://swapi.co/api/films/2/"],
+        species: ["https://swapi.co/api/species/1/"],
+        vehicles: ["https://swapi.co/api/vehicles/14/"],
+        starships: ["https://swapi.co/api/vehicles/14/"],
     }
 ]
-
-// 	"vehicles": [
-// 		"https://swapi.co/api/vehicles/14/",
-// 		"https://swapi.co/api/vehicles/30/"
-// 	],
-// 	"starships": [
-// 		"https://swapi.co/api/starships/12/",
-// 		"https://swapi.co/api/starships/22/"
-// 	],
 
 mongoose.connect('mongodb://localhost/people');
 
